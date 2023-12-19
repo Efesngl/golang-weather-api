eb0c85ca0e9a3f7cccacad3f112168b2839b86fd
Efe Şengül <oyundny01@gmail.com>
2023-12-19 17:21:20 +0300

    Update api.go

--- api.go
+++ api.go
@@ -26,36 +26,29 @@ func main() {
 	var w Weather
 	weatherMap := make(map[string]string)
 	r := gin.Default()
-	r.GET("/getweather/:method", func(c *gin.Context) {
-		method := c.Param("method")
-		if getWeather(&w, weatherMap, method){
-			c.JSON(http.StatusOK, weatherMap)
-		}else{
-			c.Status(http.StatusNotFound)
-		}
-		
+	r.GET("/today", func(c *gin.Context) {
+		link := "http://api.weatherapi.com/v1/forecast.json?key=fa802e2cc1714f7281c100339231912&q=Izmir&days=1&aqi=no&alerts=no"
+		getWeather(&w,link,weatherMap)
+		c.JSON(http.StatusOK,weatherMap)
+	})
+	r.GET("/yesterday", func(c *gin.Context) {
+		yesterday := time.Now().Add(-24 * time.Hour).Format("2006-01-02")
+		link := fmt.Sprintf("http://api.weatherapi.com/v1/history.json?key=fa802e2cc1714f7281c100339231912&q=Izmir&dt=%v", yesterday)
+		getWeather(&w,link,weatherMap)
+		c.JSON(http.StatusOK,weatherMap)
 	})
 	r.Run()
 }
-func getWeather(w *Weather, wMap map[string]string, method string) bool {
-	methods:=[]string{"today","yesterday"}
-	isMethodValid:=false
-	for i := 0; i < len(methods); i++ {
-		if method==methods[i] {
-			isMethodValid=true
-		}
+func getWeather(w *Weather,link string, wMap map[string]string)  {
+	req, err := http.NewRequest("GET", link, nil)
+	if(err!=nil){
+		fmt.Println(err)
 	}
-	if !isMethodValid{
-		return false
-	}
-	link := "http://api.weatherapi.com/v1/forecast.json?key=fa802e2cc1714f7281c100339231912&q=Izmir&days=1&aqi=no&alerts=no"
-	if method == "yesterday" {
-		yesterday := time.Now().Add(-24 * time.Hour).Format("2006-01-02")
-		link = fmt.Sprintf("http://api.weatherapi.com/v1/history.json?key=fa802e2cc1714f7281c100339231912&q=Izmir&dt=%v", yesterday)
-	}
-	req, _ := http.NewRequest("GET", link, nil)
 	client := http.Client{}
-	res, _ := client.Do(req)
+	res, err := client.Do(req)
+	if(err!=nil){
+		fmt.Println(err)
+	}
 	resBody, _ := io.ReadAll(res.Body)
 	json.Unmarshal(resBody, &w)
 	wMap["İl"] = "İzmir"
@@ -63,5 +56,4 @@ func getWeather(w *Weather, wMap map[string]string, method string) bool {
 	wMap["En yüksek sıcaklık"] = fmt.Sprintf("%v", w.Forecast.Forecastday[0].Day.MaxtempC)
 	wMap["Ortalama sıcaklık"] = fmt.Sprintf("%v", w.Forecast.Forecastday[0].Day.AvgtempC)
 	defer res.Body.Close()
-	return true
 }
